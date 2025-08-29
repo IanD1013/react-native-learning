5febae8b299922f11c41b36e3f17805f9ccb4f29
IanD1013 <ian.tung1013@gmail.com>
2025-08-29 23:12:13 +1200

    Section 2 - Text

--- expo-cli-learning/App.tsx
+++ expo-cli-learning/App.tsx
@@ -1,9 +1,40 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Alert } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.tsx to start working on your app</Text>
+      <Text
+        style={{
+          textAlign: "center",
+          fontSize: 50,
+          fontWeight: "bold",
+          color: "blue",
+        }}
+      >
+        Title
+      </Text>
+
+      <Text
+        style={{
+          fontSize: 20,
+          textAlign: "justify",
+        }}
+        numberOfLines={2}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        <Text
+          style={{ color: "white", textDecorationLine: "underline" }}
+          onPress={() => Alert.alert("Text pressed!")}
+        >
+          Press Here
+        </Text>
+        Dolore aut consequuntur omnis velit architecto tempore molestias iusto
+        natus, ex, delectus nobis et tempora placeat? Ut, neque praesentium
+        repudiandae maiores ea a aliquam numquam id provident, cum eveniet
+        veritatis nobis voluptas saepe vero tenetur, eum autem consequatur?
+        Temporibus, quisquam aspernatur? Ullam corrupti dolor officia dicta
+        obcaecati laudantium quos quas molestiae!
+      </Text>
     </SafeAreaView>
   );
 }
