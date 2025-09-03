8a06d47f6c63c35e02404466a45671ee867146e4
IanD1013 <ian.tung1013@gmail.com>
2025-09-04 09:59:34 +1200

    Section 2 - useState hook

--- expo-cli-learning/App.tsx
+++ expo-cli-learning/App.tsx
@@ -1,22 +1,23 @@
-import { View } from "react-native";
-import { s, vs, scale, verticalScale } from "react-native-size-matters";
+import { View, Text, Button } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [state, setState] = useState(20);
+
+  const IncreaseValue = () => setState((prev) => prev + 1);
+  const DecreaseValue = () => setState((prev) => prev - 1);
+
   return (
     <View
       style={{
         backgroundColor: "white",
         flex: 1,
+        justifyContent: "center",
       }}
     >
-      <View
-        style={{
-          width: scale(300),
-          height: vs(600),
-          backgroundColor: "tomato",
-        }}
-      />
+      <Button title="Increase" onPress={IncreaseValue} />
+      <Text style={{ fontSize: 100 }}>{state}</Text>
+      <Button title="Decrease" onPress={DecreaseValue} />
     </View>
   );
 }
-``;
