8b474ce3b17e0f4604dbd12d6d9eb2b235cabf9c
IanD1013 <ian.tung1013@gmail.com>
2025-09-04 10:13:36 +1200

    Section 2 - expo icons

--- expo-cli-learning/App.tsx
+++ expo-cli-learning/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, SafeAreaView, Modal, Text } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function App() {
   const [modalVisible, setModalVisible] = useState(false);
@@ -17,6 +18,12 @@ export default function App() {
       <Modal visible={modalVisible} animationType="slide">
         <Text style={{ fontSize: 50, marginTop: 50 }}>Modal is opened</Text>
         <Button title="Close Model" onPress={() => setModalVisible(false)} />
+        <AntDesign
+          name="closecircleo"
+          size={24}
+          color="black"
+          onPress={() => setModalVisible(false)}
+        />
       </Modal>
     </SafeAreaView>
   );
