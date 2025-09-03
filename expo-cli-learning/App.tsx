8fd838f023a5317a003c18559b69102da3a913ec
IanD1013 <ian.tung1013@gmail.com>
2025-09-03 22:22:18 +1200

    Section 2 - Flex direction

--- expo-cli-learning/App.tsx
+++ expo-cli-learning/App.tsx
@@ -6,11 +6,14 @@ export default function App() {
       style={{
         backgroundColor: "white",
         flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <View style={{ flex: 2, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 1, backgroundColor: "blue" }}></View>
-      <View style={{ flex: 1, backgroundColor: "gold" }}></View>
+      <View style={{ width: 100, height: 100, backgroundColor: "tomato" }} />
+      <View style={{ width: 100, height: 100, backgroundColor: "blue" }} />
+      <View style={{ width: 100, height: 100, backgroundColor: "gold" }} />
     </View>
   );
 }
