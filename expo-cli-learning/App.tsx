22dbee78eccdf6becb76e55b085b5b73ee9e2d41
IanD1013 <ian.tung1013@gmail.com>
2025-09-03 22:12:25 +1200

    Section 2 - Flex

--- expo-cli-learning/App.tsx
+++ expo-cli-learning/App.tsx
@@ -2,52 +2,15 @@ import { StyleSheet, SafeAreaView, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.view1}>
-        <Text style={styles.text}>Hello</Text>
-        <Text style={styles.text}>Text 2</Text>
-      </View>
-
-      <View style={styles.view2}>
-        <View style={styles.childView}></View>
-      </View>
-
-      <View style={styles.view3}></View>
-    </SafeAreaView>
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+      }}
+    >
+      <View style={{ flex: 2, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 1, backgroundColor: "blue" }}></View>
+      <View style={{ flex: 1, backgroundColor: "gold" }}></View>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  view1: {
-    height: 200,
-    width: 200,
-    backgroundColor: "#ff9485",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  view2: {
-    backgroundColor: "black",
-    height: 150,
-    width: 150,
-    margin: 40,
-    marginLeft: 20, // will override only margin regardless of appearance order
-    paddingTop: 30, // padding add spaces inside; margin outside
-  },
-  view3: {
-    backgroundColor: "red",
-    height: 150,
-    width: 150,
-  },
-  childView: {
-    backgroundColor: "red",
-    height: 50,
-    width: 50,
-  },
-  text: {
-    fontSize: 50,
-  },
-});
